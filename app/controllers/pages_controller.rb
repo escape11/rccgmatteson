3abcc8e32d0575b2d7aff6ac86d64f51ddc9b7f4
escape11 <nikky.sqe@gmail.com>
2018-06-19 21:00:01 -0500

    imsgs

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,20 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def About
+  end
+
+  def Contact
+  end
+  
+   def Gallery
+  end
+
+
+   def Interact
+  end
+
+   def Pastors_Desk
+  end
 end
