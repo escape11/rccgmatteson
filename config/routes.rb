1649b7bbe6f5ed016274bc75d60e406469899c54
escape11 <nikky.sqe@gmail.com>
2018-06-20 19:16:06 -0500

    added working carousel and images

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
-   get 'About' => 'pages#About'
-   get 'Pastors_Desk' => 'pages#Pastors_Desk'
-   get 'Interact' => 'pages#Interact'
-   get 'Gallery' => 'pages#Gallery'
-   get 'Contact' => 'pages#Contact'
+   get 'pages/About', to: 'pages#About'
+   get 'pages/Pastors_Desk', to: 'pages#Pastors_Desk'
+   get 'pages/Interact', to: 'pages#Interact'
+   get 'pages/Gallery', to:'pages#Gallery'
+   get 'pages/Contact', to:'pages#Contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
