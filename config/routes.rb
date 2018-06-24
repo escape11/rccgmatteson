cd448f5e2b72a838864a406ac749caaaabaaf513
escape11 <nikky.sqe@gmail.com>
2018-06-23 22:06:36 -0500

    added Welcome,Information, Our Story

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root 'pages#home'
+   get 'pages/Welcome', to: 'pages#Welcome'
+   get 'pages/Our_Story', to: 'pages#Our_Story'
+    get 'pages/Information', to: 'pages#Information'
+    get 'pages/Values', to: 'pages#Values'
    get 'pages/About', to: 'pages#About'
    get 'pages/Pastors_Desk', to: 'pages#Pastors_Desk'
    get 'pages/Interact', to: 'pages#Interact'
